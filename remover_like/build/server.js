cc56cadc74053c1f16c865203eb405417b66a8ca
Joao Vitor Lima Lago Santos <vorque45@gmail.com>
2022-01-06 23:19:21 -0300

    upando a build

---
+++ remover_like/build/server.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+require("express-async-errors");
+require("./connection/connection");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const route_1 = __importDefault(require("./route/route"));
+const app = (0, express_1.default)();
+const port = 3007;
+app.use((0, cors_1.default)());
+app.use(route_1.default);
+app.use(express_1.default.urlencoded({ extended: false }));
+app.listen(port, () => console.log("Service remover like running."));
